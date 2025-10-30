66ea94367b7dc33854c53bf380a756ef38ab9e08
Rishabh Pandey <115353162+rishabhpandey30@users.noreply.github.com>
2025-10-31 00:23:38 +0530

    Time: 33 ms (83.44%), Space: 18.6 MB (54.26%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -29,44 +29,67 @@ public:
         // return NULL;
 
 // Method 2-> counting the node of both and then shift the smaller node and then starting the traversal of the both node at same
+        // ListNode* temp1 = headA;
+        // ListNode* temp2 = headB;
+        // int count1=0;
+        // while(temp1!=NULL){
+        //     temp1=temp1->next;
+        //     count1++;
+        // }
+        // int count2=0;
+        // while(temp2!=NULL){
+        //     temp2=temp2->next;
+        //     count2++;
+        // }
+        // temp1 = headA;
+        // temp2 = headB;
+
+        // // we can make the function of this code
+        // if(count1<count2){
+        //     int shift = count2-count1;
+        //     while(shift){
+        //         temp2=temp2->next;
+        //         shift--;
+        //     }
+        //     while(temp1!=temp2){
+        //     temp1=temp1->next;
+        //     temp2=temp2->next;
+        // }
+        // return temp1;
+        // }
+        // else{
+        //     int shift = count1-count2;
+        //     while(shift){
+        //         temp1=temp1->next;
+        //         shift--;
+        //     }
+        //     while(temp1!=temp2){
+        //     temp1=temp1->next;
+        //     temp2=temp2->next;
+        // }
+        // return temp1;
+        // }
+        // return NULL;
+
+        //Method 3-> Optimal Solution
+        if(headA == NULL ||  headB ==NULL){
+            return NULL;
+        }
         ListNode* temp1 = headA;
         ListNode* temp2 = headB;
-        int count1=0;
-        while(temp1!=NULL){
+        while(temp1!=temp2){
             temp1=temp1->next;
-            count1++;
-        }
-        int count2=0;
-        while(temp2!=NULL){
             temp2=temp2->next;
-            count2++;
-        }
-        temp1 = headA;
-        temp2 = headB;
-        if(count1<count2){
-            int shift = count2-count1;
-            while(shift){
-                temp2=temp2->next;
-                shift--;
+            if(temp1==temp2){
+                return temp1;
             }
-            while(temp1!=temp2){
-            temp1=temp1->next;
-            temp2=temp2->next;
+            if(temp1==NULL){
+                temp1=headB;
+            }
+            if(temp2==NULL){
+                temp2=headA;
+            }
         }
         return temp1;
-        }
-        else{
-            int shift = count1-count2;
-            while(shift){
-                temp1=temp1->next;
-                shift--;
-            }
-            while(temp1!=temp2){
-            temp1=temp1->next;
-            temp2=temp2->next;
-        }
-        return temp1;
-        }
-        return NULL;
     }
 };
