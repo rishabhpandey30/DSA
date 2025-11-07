dfb7d8d862fdbd7f0787cc26b51ed54fd733a4af
Rishabh Pandey <115353162+rishabhpandey30@users.noreply.github.com>
2025-11-07 23:58:29 +0530

    Time: 7 ms (52.55%), Space: 14.8 MB (96.19%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,20 +17,52 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        map<Node*, Node*> mpp;
-        Node* temp = head;
+        // Method 1 -- using hash map T.C - O(2n), S.C-O(n)
+    //     map<Node*, Node*> mpp;
+    //     Node* temp = head;
+    //     while(temp!=NULL){
+    //         Node* newNode = new Node(temp->val);
+    //         mpp[temp] = newNode;
+    //         temp = temp->next;
+    //     }
+    // temp = head;
+    //     while(temp!=NULL){
+    //         Node* copy = mpp[temp];
+    //         copy->next = mpp[temp->next];
+    //         copy->random = mpp[temp->random];
+    //         temp= temp->next;
+    //     }
+    //     return mpp[head];
+
+    //Method 2 -- inseting in between the nodes
+        Node* temp =head;
         while(temp!=NULL){
-            Node* newNode = new Node(temp->val);
-            mpp[temp] = newNode;
-            temp = temp->next;
+           Node* copyNode = new Node(temp->val);
+            copyNode->next = temp->next;
+            temp->next = copyNode;
+            temp =temp->next->next;
         }
-    temp = head;
+        temp =head;
         while(temp!=NULL){
-            Node* copy = mpp[temp];
-            copy->next = mpp[temp->next];
-            copy->random = mpp[temp->random];
-            temp= temp->next;
+            Node* copyNode = temp->next;
+            if(temp->random){
+                copyNode->random = temp->random->next;
+            }
+            else{
+                copyNode->random = NULL;
+            }
+            temp = temp->next->next;
         }
-        return mpp[head];
+        Node* dummy = new Node(-1);
+        Node* res = dummy;
+        temp = head;
+        while(temp!=NULL){
+            res->next = temp->next;
+            temp->next = temp->next->next;
+            res = res->next;
+            temp =temp->next;
+
+        }
+        return dummy->next;
     }
 };
