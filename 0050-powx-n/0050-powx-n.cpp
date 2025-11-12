7daf30d64dc75d927808868c4033b87c67cff149
Rishabh Pandey <115353162+rishabhpandey30@users.noreply.github.com>
2025-11-12 21:57:53 +0530

    Time: 0 ms (100%), Space: 8.6 MB (44.07%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,24 +1,41 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        double ans =1.0;
-        long nn=n;
-        if(n<0){
-            nn = -1*nn;
+        // double ans =1.0;
+        // long nn=n;
+        // if(n<0){
+        //     nn = -1*nn;
+        // }
+        // while(nn>0){
+        //     if(nn%2==1){
+        //         ans = ans*x;
+        //         nn = nn-1;
+        //     }
+        //     else{
+        //         x = x*x;
+        //         nn= nn/2;
+        //     }
+        // }
+        // if(n<0){
+        //     ans = (double)1.0/ans;
+        // }
+        // return ans;
+
+        // With Recursion
+         if (n == 0) return 1.0;
+        long long nn=n;
+        // Handle negative powers
+        if (n < 0) {
+            x = 1 / x;
+            nn= -1*nn;
         }
-        while(nn>0){
-            if(nn%2==1){
-                ans = ans*x;
-                nn = nn-1;
-            }
-            else{
-                x = x*x;
-                nn= nn/2;
-            }
-        }
-        if(n<0){
-            ans = (double)1.0/ans;
-        }
-        return ans;
+
+        // Recursive step (divide and conquer)
+        double half = myPow(x, nn / 2);
+
+        if (n % 2 == 0)
+            return half * half;
+        else
+            return half * half * x;
     }
 };
