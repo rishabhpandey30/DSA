66298be19177ef0beffb48703cb6430e1d4808d3
Rishabh Pandey <115353162+rishabhpandey30@users.noreply.github.com>
2025-11-15 23:34:48 +0530

    Time: 3 ms (71.79%), Space: 15.8 MB (30.01%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,37 +1,53 @@
 class Solution {
 public:
-    bool valid(string s){
-        int count=0;
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='('){
-                count++;
-            }
-            else{
-                count--;
-                if(count<0){
-                    return 1;
-                }
-            }
-        }
-        return count;
-    }
-    void create(int n, string s, vector<string>& str ){
-       if(s.size()==(2*n)){
-        if(!valid(s)){
+    // bool valid(string s){
+    //     int count=0;
+    //     for(int i=0;i<s.size();i++){
+    //         if(s[i]=='('){
+    //             count++;
+    //         }
+    //         else{
+    //             count--;
+    //             if(count<0){
+    //                 return 1;
+    //             }
+    //         }
+    //     }
+    //     return count;
+    // }
+    // void create(int n, string s, vector<string>& str ){
+    //    if(s.size()==(2*n)){
+    //     if(!valid(s)){
+    //         str.push_back(s);
+    //     }
+    //     return;
+    //    }
+    
+    //    create(n,s+'(',str);
+
+    //    create(n,s+')',str);
+
+    // }
+
+    //Method -2 
+    void create(int n, string s, vector<string>& str , int open, int close){
+        if(s.size()==2*n){
             str.push_back(s);
+            return;
         }
-        return;
-       }
-       s.push_back('(');
-       create(n,s,str);
-       s.pop_back();
-       s.push_back(')');
-       create(n,s,str);
-       s.pop_back();
+
+        if(open<n){
+            create(n,s+"(",str,open+1,close);
+        }
+        if(close<open){
+            create(n,s+")",str, open, close+1);
+        }
     }
     vector<string> generateParenthesis(int n) {
         vector<string> str;
-        create(n, "", str);
+        int open =0;
+        int close =0;
+        create(n, "", str,open,close);
         return str;
     }
 };
