f6e5b9f749a696257f60941d17d5d2355bd44de7
Rishabh Pandey <115353162+rishabhpandey30@users.noreply.github.com>
2025-12-12 22:51:37 +0530

    Time: 0 ms (100%), Space: 9.9 MB (80.82%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -11,9 +11,27 @@ public:
         create(ans,nums,i+1,subset);
     }
     vector<vector<int>> subsets(vector<int>& nums) {
+        // vector<vector<int>> ans;
+        // vector<int> subset;
+        //  create(ans,nums,0,subset);
+        // return ans;
+
+        //bit power set
+        int n = nums.size();
+        int subset = 1 << n;                 // total subsets = 2^n
         vector<vector<int>> ans;
-        vector<int> subset;
-         create(ans,nums,0,subset);
+
+        for(int num = 0; num < subset; num++){   // each mask from 0 to 2^n-1
+            vector<int> list;
+
+            for(int i = 0; i < n; i++){
+                // Check if the i-th bit is set in the current mask
+                if(num & (1 << i))              
+                    list.push_back(nums[i]);     // If set, include the element
+            }
+
+            ans.push_back(list);                 // add subset to answer
+        }
         return ans;
     }
 };
