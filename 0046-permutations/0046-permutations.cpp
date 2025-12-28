61e3d49fe25a11a2159a1ac6c0107e3cb27d5250
Rishabh Pandey <115353162+rishabhpandey30@users.noreply.github.com>
2025-12-29 00:09:24 +0530

    Time: 0 ms (100%), Space: 10.5 MB (86.98%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,29 +1,48 @@
 class Solution {
 public:
-    void create(vector<int>& nums, vector<int> ds, vector<vector<int>>& ans, int feq[]){
-        if(ds.size()==nums.size()){
-            ans.push_back(ds);
+ // Approch 1
+    // void create(vector<int>& nums, vector<int> ds, vector<vector<int>>& ans, int feq[]){
+    //     if(ds.size()==nums.size()){
+    //         ans.push_back(ds);
+    //         return;
+    //     }
+    //     for(int i=0;i<nums.size();i++){
+    //         if(!feq[i]){
+    //             ds.push_back(nums[i]);
+    //             feq[i]=1;
+    //             create(nums,ds,ans,feq);
+    //             ds.pop_back();
+    //             feq[i]=0;
+    //         }
+    //     }
+
+    // }
+    // vector<vector<int>> permute(vector<int>& nums) {
+    //     vector<vector<int>> ans;
+    //     vector<int> ds;
+    //     int feq[nums.size()];
+    //     for(int i=0;i<nums.size();i++){
+    //         feq[i]=0;
+    //     }
+    //         create(nums,ds, ans,feq);
+    //         return ans;
+// }
+
+// Approch 2(swaping)
+    void create(vector<int>& nums, int idx, vector<vector<int>>& ans){
+        if(idx == nums.size()){
+            ans.push_back(nums);
             return;
         }
-        for(int i=0;i<nums.size();i++){
-            if(!feq[i]){
-                ds.push_back(nums[i]);
-                feq[i]=1;
-                create(nums,ds,ans,feq);
-                ds.pop_back();
-                feq[i]=0;
-            }
+        for(int i=idx; i<nums.size();i++){
+            swap(nums[idx], nums[i]);
+            create(nums, idx+1, ans);
+            swap(nums[idx], nums[i]);
         }
-
     }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int> ds;
-        int feq[nums.size()];
-        for(int i=0;i<nums.size();i++){
-            feq[i]=0;
-        }
-            create(nums,ds, ans,feq);
-            return ans;
+        create(nums,0,ans);
+        return ans;
     }
 };
