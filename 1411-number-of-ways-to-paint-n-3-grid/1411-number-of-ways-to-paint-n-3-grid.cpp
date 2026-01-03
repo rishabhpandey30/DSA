1f866316b8619e3daf79149235ab05a733b9c45f
Rishabh Pandey <115353162+rishabhpandey30@users.noreply.github.com>
2026-01-04 00:08:30 +0530

    Time: 268 ms (14.67%), Space: 91.8 MB (9.33%) - LeetHub

--- 1411-number-of-ways-to-paint-n-3-grid/1411-number-of-ways-to-paint-n-3-grid.cpp
+++ 1411-number-of-ways-to-paint-n-3-grid/1411-number-of-ways-to-paint-n-3-grid.cpp
@@ -1,16 +1,53 @@
 class Solution {
 public:
-    int numOfWays(int n) {
-        const int MOD = 1000000007;
-        long long x = 6, y = 6;
+    // int numOfWays(int n) {
+    //     const int MOD = 1000000007;
+    //     long long x = 6, y = 6;
 
-        for (int i = 2; i <= n; i++) {
-            long long new_x = (3 * x + 2 * y) % MOD;
-            long long new_y = (2 * x + 2 * y) % MOD;
-            x = new_x;
-            y = new_y;
+    //     for (int i = 2; i <= n; i++) {
+    //         long long new_x = (3 * x + 2 * y) % MOD;
+    //         long long new_y = (2 * x + 2 * y) % MOD;
+    //         x = new_x;
+    //         y = new_y;
+    //     }
+
+    //     return (x + y) % MOD;
+    // }
+
+    //DP with backtracking
+    int solve(int i, int n, int prev1, int prev2, int prev3, int mod,
+              vector<vector<vector<vector<int>>>>& dp) {
+        if (i == n) {
+            return 1;
         }
-
-        return (x + y) % MOD;
+        if (dp[i][prev1][prev2][prev3] != -1) {
+            return dp[i][prev1][prev2][prev3];
+        }
+        int ans = 0;
+        for (int col1 = 1; col1 <= 3; col1++) {
+            if (col1 == prev1) {
+                continue;
+            }
+            for (int col2 = 1; col2 <= 3; col2++) {
+                if (col2 == prev2 || col2 == col1) {
+                    continue;
+                }
+                for (int col3 = 1; col3 <= 3; col3++) {
+                    if (col3 == prev3 || col3 == col2) {
+                        continue;
+                    }
+                    ans = (ans + solve(i + 1, n, col1, col2, col3, mod, dp)) %
+                          mod;
+                }
+            }
+        }
+        return dp[i][prev1][prev2][prev3] = ans;
+    }
+    int numOfWays(int n) {
+        int mod = 1e9 + 7;
+        vector<vector<vector<vector<int>>>> dp(
+            n, vector<vector<vector<int>>>(
+                   4, vector<vector<int>>(4, vector<int>(4, -1))));
+        return solve(0, n, 0, 0, 0, mod, dp);
     }
 };
