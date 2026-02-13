f4862ca46609df343788e81a0896b01a7cc42d76
Rishabh Pandey <115353162+rishabhpandey30@users.noreply.github.com>
2026-02-14 00:26:47 +0530

    Time: 186 ms (49.22%), Space: 132.5 MB (15.57%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -3,7 +3,7 @@ public:
     int countPrimes(int n) {
         vector<int> prime(n+1,1);
         int count=0;
-        for(int i=2;i<=n;i++){
+        for(int i=2;i<n;i++){
             if(prime[i]==1){
                 for(int j=2*i;j<=n;j+=i){
                     prime[j]=0;
